b0fcad6a7a3e555055fa9aa0641b01303c78399d
AnyaGalkina <anna.blackbird1@gmail.com>
2022-09-04 00:13:06 +0300

    Add Title component

---
+++ src/ components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from "./Title.module.css";
+
+const Title = ({title}) => {
+    return (
+        <div  className={style.title}>
+            <h2>{title}</h2>
+        </div>
+    )
+};
+
+export default Title;
