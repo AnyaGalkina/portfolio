a0789025d9ba1db66642d27b513dcafd9240cd89
AnyaGalkina <anna.blackbird1@gmail.com>
2022-09-07 00:42:46 +0300

    Fix media and css

--- src/ components/Main/Main.jsx
+++ src/ components/Main/Main.jsx
@@ -3,25 +3,21 @@ import style from "./Main.module.scss";
 import styleContainer from "../../common/styles/Container.module.scss";
 import portfolioPhoto from "../../assets/images/gitPhoto.jpeg";
 import styleLink from "../../common/styles/Link.module.scss";
-import SocialMediaLinks from "../SocialMediaLinks/SocialMediaLinks";
 
 
-const Main = ({contacts}) => {
+const Main = () => {
     return (
         <div className={style.mainBlock} id={"about"}>
             <div className={styleContainer.container}>
-                    <div className={style.photo}>
-                        <img className={style.photo} src={portfolioPhoto} alt={"my portrait photography"}/>
-                    </div>
-                    <div className={style.text}>
-                        <span className={style.greeting}>Hello,</span>
-                        <h1>I'm Anna Galkina</h1>
-                        <h5>Frontend Developer</h5>
-                        <span>some other text Placeholder////</span>
-                        <a className={styleLink.link}>Download CV</a>
-                    </div>
-                <div className={style.iconContainer}>
-                    <SocialMediaLinks contacts={contacts}/>
+                <div className={style.photo}>
+                    <img className={style.photo} src={portfolioPhoto} alt={"my portrait photography"}/>
+                </div>
+                <div className={style.text}>
+                    <span className={style.greeting}>Hello,</span>
+                    <h1>I'm Anna Galkina</h1>
+                    <h5>Frontend Developer</h5>
+                    <span>some other text Placeholder////</span>
+                    <a className={styleLink.link}>Download CV</a>
                 </div>
             </div>
         </div>
--- src/ components/Projects/Project/Project.jsx
+++ src/ components/Projects/Project/Project.jsx
@@ -7,14 +7,12 @@ const Project = (props) => {
 
     return (
         <div className={style.projectContainer}>
-            {/*<div>*/}
                 <div className={style.imgContainer} style={{backgroundImage: `url(${props.src})`}}>
                     <a
                         href={props.projectLink}
                         className={`${styleLink.link} ${style.link}`}
                     >Go to project</a>
                 </div>
-            {/*</div>*/}
             <h3>{props.title}</h3>
             <span className={style.projectDescription}>{props.description}</span>
         </div>
