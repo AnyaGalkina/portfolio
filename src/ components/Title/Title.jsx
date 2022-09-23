2ee28001d51a2048e49b32b97dc4818b69bb4d22
AnyaGalkina <anna.blackbird1@gmail.com>
2022-09-23 14:21:31 +0300

    Add title fade effect

--- src/ components/Title/Title.jsx
+++ src/ components/Title/Title.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from "./Title.module.scss";
+import Fade from 'react-reveal/Fade';
 
 const Title = ({title}) => {
     return (
-        <div  className={style.title}>
-            <h2>{title}</h2>
+        <div className={style.title}>
+            <Fade top>
+                <h2>{title}</h2>
+            </Fade>
         </div>
     )
 };
