4e2bf599d74c8e8b0dbd559e80c0d83d553585c1
AnyaGalkina <anna.blackbird1@gmail.com>
2022-10-17 22:20:43 +0300

    Add theme

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {HashRouter} from "react-router-dom";
+import {ThemeProvider} from "@mui/material";
+import {theme} from "./common/theme";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HashRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+
     </HashRouter>
 );
 
