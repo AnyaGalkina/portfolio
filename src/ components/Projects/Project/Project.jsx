2c8068f1dffc80095fcd88f96a82e2040b9a2482
AnyaGalkina <anna.blackbird1@gmail.com>
2022-11-09 13:53:43 +0300

    Add align-self for skill container. Add divider to Project.

--- src/ components/Projects/Project/Project.jsx
+++ src/ components/Projects/Project/Project.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import style from "./Project.module.scss";
 import {ImgCarusel} from "./ImgCarousel/ImgCarousel";
+import {Divider} from "@mui/material";
 
 
-const Project = ({imgSrc, title, main, description, technicalStack, gitHubLink, projectLink, mobileResponsive}) => {
+const Project = ({imgSrc, title, main, description, technicalStack, gitHubLink, projectLink}) => {
 
     return (
         <div className={style.projectContainer}>
             <ImgCarusel images={imgSrc}/>
             <h3 className={style.title}>{title}</h3>
 
-
+            <Divider className={style.divider}/>
             <span className={style.projectDescription}><b>{main}</b> {description}</span>
             <span className={style.projectDescription}><b>Technologies used: </b>{technicalStack}</span>
             <span className={style.projectDescription}><b>Github link: </b>
